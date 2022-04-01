6f548247e747ec24746f56ec55a3cf72fd8c6bed
Arjun Raja Yogidas <arjunbirju@Arjuns-MacBook-Air.local>
2022-03-31 21:21:38 -0400

    Assignment 4 submitted 31st march, find report in Reports folder

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -15,15 +15,16 @@ import java.util.concurrent.ForkJoinPool;
  * TODO tidy it up a bit.
  */
 public class Main {
-
+    public static int threadCount = 2;
+    public static ForkJoinPool myPool = new ForkJoinPool(threadCount);
     public static void main(String[] args) {
         processArgs(args);
-        System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
+        System.out.println("Degree of parallelism: " + myPool.getParallelism());
         Random random = new Random();
-        int[] array = new int[2000000];
+        int[] array = new int[16000000];
         ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
+        for (int j = 0; j < 20; j++) {
+            ParSort.cutoff = 800000 * (j + 1);
             // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
             long time;
             long startTime = System.currentTimeMillis();
